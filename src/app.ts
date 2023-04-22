d0b4290ad53158c4593dcc5578cda502d19965e6
eran <eranavru100@gmail.com>
2023-04-22 15:55:13 +0300

    real time update + code stay after refresh

--- src/app.ts
+++ src/app.ts
@@ -3,15 +3,97 @@ import { connectToDB } from "./connection";
 import routes from "./routes/index";
 import bodyParser from "body-parser";
 import cors from "cors";
+import { getAllRooms } from "./controller/room.controller";
+import { getRooms } from "./service/room.service";
+import { IRoom } from "./models/room.model";
+import { Server } from "socket.io";
 
 const app = express();
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use(cors());
 
-
-
 app.use(routes);
-app.listen(8000, () => console.log("Listening..."));
+const server = app.listen(8000, () => console.log("Listening..."));
 
-connectToDB() 
+connectToDB();
+
+interface IRoomsDetails {
+  roomTopic: string;
+  amountOfUsers: number;
+  mentorId?: string;
+  roomCode?: string[];
+}
+let rooms:IRoomsDetails[] = [];
+
+const getRoomFromDb = async () => {
+    const roomsFromDb: IRoom[] = await getRooms();
+   roomsFromDb.map((room: IRoom) => {
+    const oneRoom:IRoomsDetails = {
+        roomTopic: room.roomName,
+        amountOfUsers: 0,
+        mentorId: "",
+        roomCode: room.value,
+      }
+      rooms.push(oneRoom)
+   }
+       );
+ 
+};
+
+getRoomFromDb();
+
+const io = new Server(server,{
+    cors: {
+      origin: ["http://localhost:3000"],
+      methods: ["GET", "POST"],
+    },
+  }) ;
+
+io.on("connection", (socket: any) => {
+    socket.on("specific-room", (topic: string) => {
+      const room = rooms.find((e:IRoomsDetails) => e.roomTopic === topic);
+      socket.join(topic);
+      if(room){
+            socket.emit("send-code", room.roomCode)
+            room.amountOfUsers = room.amountOfUsers + 1;
+            if (room.amountOfUsers === 1 ||room.amountOfUsers === 0 ) {
+              room.mentorId = socket.id;
+            }
+        }
+    console.log('room',room);
+  });
+
+  socket.on("user-typing", (code: string, topic: string) => {
+    const room = rooms.find((e:IRoomsDetails) => e.roomTopic === topic);
+    if(room){
+        room.roomCode = code.split('\n')
+        socket.to(topic).emit("send-code", room.roomCode);
+    }
+
+});
+
+socket.on('disconnect',()=>{
+    const room = rooms.find((r) => r.roomTopic === socket.handshake.query.roomTopic);
+    if(room){
+        room.amountOfUsers = room.amountOfUsers - 1;
+    }
+    console.log('disconnect',room);
+    
+})
+  
+});
+
+// usersInRoom.map((room:IRooms) => {
+//     if(room.roomTopic === topic){
+//         room.amountOfUsers ++
+//         console.log("join",usersInRoom);
+//     }})
+//     socket.on('disconnect', () => {
+//         socket.leave(topic)
+//         usersInRoom.map((room:IRooms) => {
+//             if(room.roomTopic === topic){
+//                 room.amountOfUsers --
+//                 console.log('disconnect',usersInRoom);
+//             }})
+//   });
