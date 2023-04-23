a5de35026bd71662fe9f68460a993594ff6db1e5
eran <eranavru100@gmail.com>
2023-04-23 20:19:56 +0300

    Update Socket.ts

--- src/Socket/Socket.ts
+++ src/Socket/Socket.ts
@@ -43,7 +43,7 @@ export const socketConnection = () => {
       (r) => r.roomTopic === socket.handshake.query.roomTopic
     );
     const ip = socket.handshake.headers['x-forwarded-for'].split(',')[0];
-    console.log("ipAddress", ip);
+    
     if((room?.clientConnect.findIndex((e) =>e===ip)) === -1)
     room?.clientConnect.push(ip);
     console.log("clients",room?.clientConnect);
