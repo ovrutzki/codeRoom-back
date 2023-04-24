6bbac9fd3f3c659a20d26150dd94a5f7616c3d64
eran <eranavru100@gmail.com>
2023-04-24 09:12:19 +0300

    Update Socket.ts

--- src/Socket/Socket.ts
+++ src/Socket/Socket.ts
@@ -87,7 +87,6 @@ export const socketConnection = () => {
     socket.on("user-typing", (code: string[], topic: string) => {
       if (room) {
         room.roomCode = code;
-        console.log("code",code);    
         socket.broadcast.to(topic).emit("send-code", code);
       }
     });
