e0ccdb625ecd30e7438d4b6b8c82531356d1dac0
eran <eranavru100@gmail.com>
2023-04-24 10:50:44 +0300

    final update

--- src/Socket/Socket.ts
+++ src/Socket/Socket.ts
@@ -14,6 +14,7 @@ interface IRoomsDetails {
 export const socketConnection = () => {
   let rooms: IRoomsDetails[] = [];
 
+// take room data from data base when the server run
   const getRoomFromDb = async () => {
     const roomsFromDb: IRoom[] = await getRooms();
     roomsFromDb.map((room: IRoom) => {
@@ -29,7 +30,7 @@ export const socketConnection = () => {
   };
 
   getRoomFromDb();
-
+// create the socket connection
   const io = new Server(server, {
     cors: {
       origin: ['https://eran-coderoom.onrender.com'],
@@ -44,28 +45,29 @@ export const socketConnection = () => {
     );
     const ip = socket.handshake.headers['x-forwarded-for'].split(',')[0];
 
+    // if the client not in the list of the client insert him:
     if((room?.clientConnect.findIndex((e) =>e===ip)) === -1)
     room?.clientConnect.push(ip);
-    console.log("clients",room?.clientConnect);
     
     // send the ip address to the client:
     socket.emit("ip-address", ip);
 
+    // join a specific room when enter to it`s page:
     socket.on("specific-room", (topic: string) => {
       socket.join(topic);
       if (room) {
         if (room.clientConnect[0] === ip) {
           room.mentorId = ip;
-          console.log(ip, "cc", room.mentorId);
           
         }
+        // send to the user the code from the data base and the ip of the mentor:
         socket.emit("send-code", room.roomCode, 'DataBase');
         socket.emit("mentor-id", room.mentorId);
         room.amountOfUsers = room.amountOfUsers + 1;
-        console.log("++1", room.roomTopic, room.amountOfUsers);
       }
     });
 
+    // when "im not a mentor" clicked - reset the mentorId value
     socket.on("not-mentor", () => {
       if (room) {
         room.mentorId = "";
@@ -74,6 +76,7 @@ export const socketConnection = () => {
         clientId && room.clientConnect.push(clientId)
       }
     });
+        // when "im a mentor" clicked - make the user mentor and if there is another mentor -> make him a user
     socket.on('is-mentor', ()=>{
         if(room){
             const ipIndex = room.clientConnect.findIndex((e)=> e===ip);
@@ -83,31 +86,20 @@ export const socketConnection = () => {
         socket.to(room?.roomTopic).emit('not-mentor')
     })
 
-
+// get what the user is typing and send it to the users in the specific room:
     socket.on("user-typing", (code: string[],userName:string ,topic: string) => {
       if (room) {
         room.roomCode = code;
-        console.log(userName);
         socket.broadcast.to(topic).emit("send-code", code, userName);
       }
     });
-
+// disconnect and delete user from user list
     socket.on("disconnect", () => {
       if(room){
-        const ipIndex = room.clientConnect.findIndex((e)=> e===ip)
-        console.log("disconnect" , room.clientConnect[ipIndex]);
-        
+        const ipIndex = room.clientConnect.findIndex((e)=> e===ip)        
         delete room.clientConnect[ipIndex]
       }
-    //   if (room && room.amountOfUsers > 1) {
-    //     room.amountOfUsers = room.amountOfUsers - 2;
-    //     console.log("0>", room.roomTopic, room.amountOfUsers);
-    //   } else if (room && room.amountOfUsers === 0) {
-    //     // room.mentorId = '';
-    //     console.log("number", room.roomTopic, room.amountOfUsers);
-    //   } else if (room && room.amountOfUsers === 1) {
-    //     room.amountOfUsers = room.amountOfUsers - 1;
-    //   }
+   
     });
   });
 };
